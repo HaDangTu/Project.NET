0f2fb0e174d12b86e54e3fe37c1e24a906060723
HaDangTu <38284966+HaDangTu@users.noreply.github.com>
2020-04-23 14:08:50 +0700

    update code mẫu

--- Project_end_semester/MotelManagement/Controllers/PowerController.cs
+++ Project_end_semester/MotelManagement/Controllers/PowerController.cs
@@ -13,6 +13,7 @@ namespace MotelManagement.Controllers
 {
     public class PowerController : Controller
     {
+        //Thuộc tính để lấy data trong database lên
         private ApplicationDbContext _dbContext;
 
         public PowerController()
@@ -20,24 +21,42 @@ namespace MotelManagement.Controllers
             _dbContext = new ApplicationDbContext();
         }
 
-        // GET: Update Power
+        /* GET: Power/Update
+         * 
+         */
         public ActionResult Update()
         {
+            /* Sử dụng viewmodel/model tại đây đều được
+             * nếu model thông thường không đáp ứng được dữ liệu để show trên view thì sử dụng view model
+             * vd trên form thêm một học sinh cần phải có dữ liệu để fill vào combox lớp đê người dùng chọn lớp cho học sinh
+             * thì ta tạo StudentViewModel có thuộc tính List<Course> Courses để có thể fill data vào combobox
+             */ 
             PowerInfoViewModel viewModel = new PowerInfoViewModel()
             {
+                /* Sử dụng LINQ đê lọc ra những phòng chưa đc cập nhật chỉ số điện, nước
+                 */
                 Rooms = _dbContext.Rooms.Include(i => i.Infos)
                 .Where(r => r.Guests.Count(g => g.StateID == "S01") > 0 &&
-                    r.Infos.Count(i => i.Date.Month == (DateTime.Now.Month)) < 1)
+                    r.Infos.Count(i => i.Date.Month == (DateTime.Now.Month)) < 1) 
             };
 
+            /*
+             * Lưu ý: Khi chúng ta dùng viewModel thì kết quả khi submit form sẽ trả về là 1 viewModel 
+             * (Xem tham số của phương thức xử lý POST request bên dưới)
+             */ 
             return View(viewModel);
         }
 
+        /*  Post: Update Power
+         *  Request của client khi nhấn nút submit trên form sẽ đc xử lý tại phương thức này
+         *  ViewModel/Model sẽ lưu những thông tin mà người dùng đã nhập trên form của page.
+         */
         [HttpPost]
         //[Authorize(Roles = "Owner")]
         [ValidateAntiForgeryToken]
         public ActionResult Update(PowerInfoViewModel viewModel)
         {
+            //Tạo next Id
             string nextInfosId = string.Empty;
             IEnumerable<ElectricityAndWaterInfo> infos = _dbContext.Infos;
 
@@ -46,6 +65,7 @@ namespace MotelManagement.Controllers
             if (lastInfos == null) nextInfosId = "IEW200001";
             else nextInfosId = IdGenerator.generateNextID("IEW", lastInfos.ID, true);
 
+            //Kiểm tra các trường trên form đã valid hay không
             if (!ModelState.IsValid)
             {
                 PowerInfoViewModel model = new PowerInfoViewModel()
@@ -58,6 +78,7 @@ namespace MotelManagement.Controllers
                 return View(model);
             }
 
+            //Mapping dữ liệu trên form
             ElectricityAndWaterInfo info = new ElectricityAndWaterInfo()
             {
                 ID = nextInfosId,
@@ -67,6 +88,9 @@ namespace MotelManagement.Controllers
                 RoomID = viewModel.RoomID
             };
 
+            /* Insert dữ liệu vào database
+             * Sử dụng _dbContext.SaveChanges() để lưu lại các thay đổi trên CSDL (Insert, Update, Delete)
+             */
             _dbContext.Infos.Add(info);
             if (_dbContext.SaveChanges() > 0)
             {
@@ -76,6 +100,8 @@ namespace MotelManagement.Controllers
             {
                 TempData["Fail"] = "Cập nhật thông tin điện nước thất bại";
             }
+
+            //Redirect về home page hoặc bất cứ page nào mình muốn
             return RedirectToAction("Update", "Power");
         }
     }
--- Project_end_semester/MotelManagement/DAL/ApplicationDbContext.cs
+++ Project_end_semester/MotelManagement/DAL/ApplicationDbContext.cs
@@ -10,6 +10,13 @@ namespace MotelManagement.DAL
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        /* Chứa các thuộc tính dùng để truy vấn dữ liệu với database, 
+         * mỗi thuộc tính vd Genders tương đương với một bảng trong database vd bảng Genders
+         * Khi cần truy vấn dữ liệu chỉ cần gọi đến các thuộc tính này hoặc có thể sử dung LINQ để lọc dữ liệu
+         * Khi tạo ra một model mới mà model này tương đương với 1 bảng trong CSDL thì thêm model vào đây rồi sử dụng
+         * Add-Migration trong Package Console Manager. EF6 sẽ tự động viết lệnh tạo bảng, sau đó sd lệnh Update-Database 
+         * để tạo bảng mới trong CSDL
+         */
         public DbSet<Gender> Genders { get; set; }
         public DbSet<State> States { get; set; }
         public DbSet<Guest> Guests { get; set; }
