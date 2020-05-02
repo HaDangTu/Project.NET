349f0a294cacd1775985e041fd274c4405aeca3a
NGẠN TRƯƠNG TUẤN <16520796@ms.uit.edu.vn>
2020-05-02 14:32:51 +0700

    Update ReportController+ ViewModels+ View

---
+++ Project_end_semester/MotelManagement/ViewModels/AllRoomDebtViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MotelManagement.ViewModels
+{
+    public class AllRoomDebtViewModel
+    {
+        public int CountDebt { get; set; }
+
+        [Column("id")]
+        public string ID { get; set; }
+
+        [Column("name")]
+        [Display(Name = "Tên phòng")]
+        public string Name { get; set; }
+    }
+}
